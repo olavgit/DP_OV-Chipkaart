94adaefe8a01889a41e2bbe9518287a9795f2337
olavsajtos <o.sajtos@outlook.com>
2022-09-08 15:35:42 +0200

    CRUD werkt

--- src/p2/Main.java
+++ src/p2/Main.java
@@ -1,8 +1,44 @@
 package p2;
 
+import java.sql.*;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        Reiziger r1 = new Reiziger(1, "E", "van de", "Steeg", java.sql.Date.valueOf("2005-01-18"));
-        System.out.println(r1);
+    private static Connection getConnection() throws SQLException {
+        String url = "jdbc:postgresql://localhost:5432/ovchip";
+        return DriverManager.getConnection(url, "postgres", "Jopie06!");
+    }
+
+    private static void closeConnection() throws SQLException {
+        getConnection().close();
+    }
+
+    private static void testReizigerDAO(ReizigerDAO rdao) throws SQLException {
+        System.out.println("\n---------- Test ReizigerDAO -------------");
+
+        // Haal alle reizigers op uit de database
+        List<Reiziger> reizigers = rdao.findAll();
+        System.out.println("[Test] ReizigerDAO.findAll() geeft de volgende reizigers:");
+        for (Reiziger r : reizigers) {
+            System.out.println(r);
+        }
+        System.out.println();
+
+        // Maak een nieuwe reiziger aan en persisteer deze in de database
+        String gbdatum = "1981-03-14";
+        Reiziger sietske = new Reiziger(77, "S", "", "Boers", java.sql.Date.valueOf(gbdatum));
+        System.out.print("[Test] Eerst " + reizigers.size() + " reizigers, na ReizigerDAO.save() ");
+        rdao.save(sietske);
+        reizigers = rdao.findAll();
+        System.out.println(reizigers.size() + " reizigers\n");
+
+        // Voeg aanvullende tests van de ontbrekende CRUD-operaties in.
+    }
+
+    public static void main(String[] args) throws SQLException {
+        Connection conn = getConnection();
+        ReizigerDAO rdao = new ReizigerDAOPsql(conn);
+        testReizigerDAO(rdao);
+        closeConnection();
     }
 }
--- src/p2/ReizigerDAOPsql.java
+++ src/p2/ReizigerDAOPsql.java
@@ -14,11 +14,13 @@ public class ReizigerDAOPsql implements ReizigerDAO {
     @Override
     public boolean save(Reiziger reiziger) {
         try {
-            PreparedStatement pst = conn.prepareStatement("INSERT INTO reiziger VALUES(voorletters = ?, tussenvoegsel = ?, achternaam = ?, geboortedatum = ?)");
-            pst.setString(1, reiziger.getVoorletters());
-            pst.setString(2, reiziger.getTussenvoegsel());
-            pst.setString(3, reiziger.getAchternaam());
-            pst.setDate(4, reiziger.getGeboortedatum());
+            // Insert de reiziger met alle gegevens van het meegegeven object
+            PreparedStatement pst = conn.prepareStatement("INSERT INTO reiziger (reiziger_id, voorletters, tussenvoegsel, achternaam, geboortedatum) VALUES (?, ?, ?, ?, ?)");
+            pst.setInt(1, reiziger.getId());
+            pst.setString(2, reiziger.getVoorletters());
+            pst.setString(3, reiziger.getTussenvoegsel());
+            pst.setString(4, reiziger.getAchternaam());
+            pst.setDate(5, reiziger.getGeboortedatum());
             pst.executeUpdate();
             pst.close();
             return true;
@@ -31,29 +33,70 @@ public class ReizigerDAOPsql implements ReizigerDAO {
     @Override
     public boolean update(Reiziger reiziger) {
         try {
-            PreparedStatement pst = conn.prepareStatement()
+            // Update de reiziger met de id uit het meegegeven object
+            PreparedStatement pst = conn.prepareStatement("UPDATE reiziger VALUES(voorletters = ?, tussenvoegsel = ?, achternaam = ?, geboortedatum = ?) WHERE reiziger_id = ?");
+            pst.setString(1, reiziger.getVoorletters());
+            pst.setString(2, reiziger.getTussenvoegsel());
+            pst.setString(3, reiziger.getAchternaam());
+            pst.setDate(4, reiziger.getGeboortedatum());
+            pst.setInt(5, reiziger.getId());
+            pst.executeUpdate();
+            pst.close();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
         return false;
     }
 
     @Override
     public boolean delete(Reiziger reiziger) {
+        try {
+            // Delete de reiziger met de id uit het meegegeven object
+            PreparedStatement pst = conn.prepareStatement("DELETE reiziger WHERE reiziger_id = ?");
+            pst.setInt(1, reiziger.getId());
+            pst.executeUpdate();
+            pst.close();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return false;
     }
 
     @Override
     public Reiziger findById(int id) {
+        try {
+            // Selecteer de reiziger met id 'id'
+            PreparedStatement pst = conn.prepareStatement("SELECT * FROM reiziger WHERE id = ?");
+            pst.setInt(1, id);
+            ResultSet rs = pst.executeQuery();
+            Reiziger r = null;
+
+            // Maak een object voor de geselecteerde reiziger
+            while (rs.next()) {
+                r = new Reiziger(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getDate(5));
+            }
+
+            rs.close();
+            pst.close();
+            return r;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     @Override
     public List<Reiziger> findByGbDatum(String date) {
         try {
+            // Selecteer alle reizigers met geboortdatum 'date'
             PreparedStatement pst = conn.prepareStatement("SELECT * FROM reiziger WHERE geboortedatum = ?");
             pst.setDate(1, Date.valueOf(date));
             ResultSet rs = pst.executeQuery();
             List gbReizigers = new ArrayList();
 
+            // Voor elke geselecteerde reiziger, maak een object en voeg deze toe aan een lijst
             while (rs.next()) {
                 Reiziger r = new Reiziger(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getDate(5));
                 gbReizigers.add(r);
@@ -65,15 +108,18 @@ public class ReizigerDAOPsql implements ReizigerDAO {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        return null;
     }
 
     @Override
     public List<Reiziger> findAll() {
         try {
+            // Selecteer alle reizigers
             PreparedStatement pst = conn.prepareStatement("SELECT * FROM reiziger");
             ResultSet rs = pst.executeQuery();
             List alleReizigers = new ArrayList();
 
+            // Voor elke geselecteerde reiziger, maak een object en voeg deze toe aan een lijst
             while (rs.next()) {
                 Reiziger r = new Reiziger(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getDate(5));
                 alleReizigers.add(r);
@@ -85,5 +131,6 @@ public class ReizigerDAOPsql implements ReizigerDAO {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        return null;
     }
 }
